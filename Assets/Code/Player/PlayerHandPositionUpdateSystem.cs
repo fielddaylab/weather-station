ff7069e310fae24420199749d2b45d0ebf26b153
rdtredinnick <rdtredinnick@wisc.edu>
2023-10-10 11:15:35 -0500

    Made initial puzzle prefabs and re-inserted them into the test scene.

--- Assets/Code/Player/PlayerHandPositionUpdateSystem.cs
+++ Assets/Code/Player/PlayerHandPositionUpdateSystem.cs
@@ -12,14 +12,18 @@ namespace WeatherStation {
 			
             m_State.UpdateStates();
 			
-			if(m_State.LeftHandGrab.IsGrabPosed) {
-				//update rotation transform of grip pose hand to match that of the tracked if we are currently grabbing something
-				m_State.LeftHandGrab.gameObject.transform.rotation = m_State.LeftHandGrab.GrabberVisual.transform.rotation;
+			if(m_State.LeftHandGrab != null) {
+				if(m_State.LeftHandGrab.IsGrabPosed) {
+					//update rotation transform of grip pose hand to match that of the tracked if we are currently grabbing something
+					m_State.LeftHandGrab.gameObject.transform.rotation = m_State.LeftHandGrab.GrabberVisual.transform.rotation;
+				}
 			}
 			
-			if(m_State.RightHandGrab.IsGrabPosed) {
-				//update rotation transform of grip pose hand to match that of the tracked if we are currently grabbing something
-				m_State.RightHandGrab.gameObject.transform.rotation = m_State.RightHandGrab.GrabberVisual.transform.rotation;
+			if(m_State.RightHandGrab != null) {
+				if(m_State.RightHandGrab.IsGrabPosed) {
+					//update rotation transform of grip pose hand to match that of the tracked if we are currently grabbing something
+					m_State.RightHandGrab.gameObject.transform.rotation = m_State.RightHandGrab.GrabberVisual.transform.rotation;
+				}
 			}
         }
     }
