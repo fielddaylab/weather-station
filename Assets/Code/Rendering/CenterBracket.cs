919aaa639bc23cbf02c06471616522a07eced8db
Ross Tredinnick <ross.tredinnick@gmail.com>
2024-01-10 12:59:15 -0600

    Center bracket movement work in progress

---
+++ Assets/Code/Rendering/CenterBracket.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BeauUtil;
+using BeauUtil.Debugger;
+using FieldDay;
+using FieldDay.Components;
+using FieldDay.SharedState;
+using FieldDay.Systems;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace WeatherStation {
+    public class CenterBracket : MonoBehaviour {
+        #region Inspector
+
+		
+        #endregion // Inspector
+		
+		private Grabbable Handle;
+		
+		private bool RightGrabbed = false;
+		private bool LeftGrabbed = false;
+		
+		[NonSerialized] public bool WasGrabbed = false;
+		
+		private Vector3 LastPos = Vector3.zero;
+		
+		void Update() {
+
+			PlayerHandRig handRig = Game.SharedState.Get<PlayerHandRig>();
+
+			Vector3 currPos = Vector3.zero;
+			Vector3 euler = transform.rotation.eulerAngles;
+			//Debug.Log(euler.ToString("F5"));
+			if(LeftGrabbed || RightGrabbed) {
+				
+				WasGrabbed = true;
+				
+				if(LeftGrabbed) {
+					currPos = handRig.LeftHand.Visual.position;
+				} else {
+					currPos = handRig.RightHand.Visual.position;
+				}
+				
+
+				float dir = -1f;
+				if(LastPos.y - currPos.y > 0f) {
+					dir = 1f;
+				}
+				
+				if(currPos.y > 0.5f && currPos.y < 2.5f) {
+					transform.Translate(Vector3.up * dir * Vector3.Distance(LastPos, currPos)*100f);
+				}
+				
+				LastPos = currPos;
+			} 
+			
+			//Debug.Log(euler.z);
+		}
+		
+        private void Awake() {
+			Handle = GetComponent<Grabbable>();
+			if(Handle != null) {
+				Handle.OnGrabbed.Register(OnGrabPanel);
+				Handle.OnReleased.Register(OnReleasePanel);
+			}
+        }
+		
+		private void OnGrabPanel(Grabber grabber) {
+			
+			PlayerHandRig handRig = Game.SharedState.Get<PlayerHandRig>();
+			
+			if(grabber == handRig.RightHand.Physics) {
+				RightGrabbed = true;
+				LastPos = handRig.RightHand.Visual.position;
+			}
+			
+			if(grabber == handRig.LeftHand.Physics) {
+				LeftGrabbed = true;
+				LastPos = handRig.LeftHand.Visual.position;
+			}	
+		}
+		
+		private void OnReleasePanel(Grabber grabber) {
+			PlayerHandRig handRig = Game.SharedState.Get<PlayerHandRig>();
+			
+			if(grabber == handRig.RightHand.Physics) {
+				RightGrabbed = false;
+			}
+			
+			if(grabber == handRig.LeftHand.Physics) {
+				LeftGrabbed = false;
+			}
+		}
+    }
+
+}
