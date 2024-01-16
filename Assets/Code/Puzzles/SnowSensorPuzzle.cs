9c00d53dbc22d9439c1a467b04cac1803b4b4dbb
rdtredinnick <rdtredinnick@wisc.edu>
2024-01-16 13:54:48 -0600

    Refactoring puzzles for levels 3 and 5

--- Assets/Code/Puzzles/SnowSensorPuzzle.cs
+++ Assets/Code/Puzzles/SnowSensorPuzzle.cs
@@ -13,29 +13,17 @@ using UnityEngine.XR;
 namespace WeatherStation {
     public class SnowSensorPuzzle : Puzzle {
         #region Inspector
-        //public List<PuzzleButton> PuzzleButtons = new List<PuzzleButton>();
-   
-        public List<Material> SensorMaterials = new List<Material>();
+        public List<Material> SensorMaterials = new List<Material>(4);
 
-		public List<Material> PairedMaterials = new List<Material>();
+		public List<Material> PairedMaterials = new List<Material>(4);
 
-        /*[Serializable]
-        public class PuzzleSlot {
-            public List<Texture2D> SlotTextures = new List<Texture2D>();
-        }
-        
-        public List<PuzzleSlot> PuzzleSlots = new List<PuzzleSlot>();*/
-        public List<Texture2D> Solution = new List<Texture2D>(); 
-        public List<Texture2D> SolutionTextures = new List<Texture2D>(); 
+		public List<Texture2D> StartingTextures = new List<Texture2D>(4);
+        public List<Texture2D> SolutionTextures = new List<Texture2D>(4); 
 
 		public Color GlowColor;
 		public Material SensorMaterial;
 
         #endregion // Inspector
-
-        //private int[] ButtonIndices;
-
-		//private List<Color> PriorColors = new List<Color>();
 		
 		const int TOTAL_BUTTONS = 4;
 
@@ -66,68 +54,12 @@ namespace WeatherStation {
         private void Awake() {
             
 			for(int i = 0; i < TOTAL_BUTTONS; ++i) {
+				SensorMaterials[i].mainTexture = StartingTextures[i];
 				//PuzzleButtons[i].OnPressed.Register(SensorButtonPressed);
 				//SensorMaterials[i].mainTexture = PuzzleSlots[i].SlotTextures[0];
 				//PriorColors.Add(PairedMaterials[i].color);
 			}
-
-			//ButtonIndices = new int[TOTAL_BUTTONS];
-
-			//for(int i = 0; i < TOTAL_BUTTONS; ++i) {
-			//	ButtonIndices[i] = 0;
-			//}
-		
         }
-
-        /*private void SensorButtonPressed(PuzzleButton button) {
-            //switch between textures...
-            int buttonIndex = (int)char.GetNumericValue(button.gameObject.name[button.gameObject.name.Length-1])-1;
-            ButtonIndices[buttonIndex]++;
-            ButtonIndices[buttonIndex] = ButtonIndices[buttonIndex] % PuzzleSlots[buttonIndex].SlotTextures.Count;
-            SensorMaterials[buttonIndex].mainTexture = PuzzleSlots[buttonIndex].SlotTextures[ButtonIndices[buttonIndex]];
-			
-			//the algorithm here should be - if pressing a button - look ahead, and any button that is locked (until you get to an incorrect button) 
-			//and part of the solution, switch to solution texture
-            //walk from beginning to current texture, and switch red to blue if all preceeding textures are good... if one isn't good, turn off all textures up to current from that one.
-	
-			if(SensorMaterials[buttonIndex].mainTexture == Solution[buttonIndex]) {
-				//check to see if all before this are on solutionTextures, if so, change this one too...
-				bool allSetBefore = true;
-				for(int i = 0; i < buttonIndex; ++i) {
-					if(SensorMaterials[i].mainTexture != SolutionTextures[i]) {
-						allSetBefore = false;
-						break;
-					}
-				}
-				
-				if(allSetBefore) {
-					SensorMaterials[buttonIndex].mainTexture = SolutionTextures[buttonIndex];
-					if(buttonIndex > 0) {
-						PairedMaterials[buttonIndex-1].color = GlowColor;
-					}
-					//if this happens, then also walk ahead and see if we can switch any that are on a solution to final..
-					for(int i = buttonIndex+1; i < PuzzleButtons.Count; ++i) {
-						if(SensorMaterials[i].mainTexture == Solution[i]) {
-							SensorMaterials[i].mainTexture = SolutionTextures[i];
-							PairedMaterials[i-1].color = GlowColor;
-						} else {
-							break;
-						}
-					}
-				}
-			} else {
-				//if we turn off.. then anything ahead of me should also go red...
-				for(int i = buttonIndex; i < PuzzleButtons.Count; ++i) {
-					if(SensorMaterials[i].mainTexture == SolutionTextures[i]) {
-						SensorMaterials[i].mainTexture = Solution[i];
-					}
-					
-					if(i > 0) {
-						PairedMaterials[i-1].color = PriorColors[i-1];
-					}
-				}
-			}
-        }*/
     }
 
 }
