02f1b106ce5fd4eb02f8efa33da8cbcc4019a6ab
Ross <rdtredinnick@wisc.edu>
2024-01-18 10:27:04 -0600

    Work in progress on level 3, argo fun button

--- Assets/Code/Audio/ArgoFun.cs
+++ Assets/Code/Audio/ArgoFun.cs
@@ -11,10 +11,10 @@ using UnityEngine.Scripting;
 
 namespace WeatherStation {
 	[RequireComponent(typeof(AudioSource))]
-public class ArgoHelp : SharedStateComponent {
+public class ArgoFun : SharedStateComponent {
 
-	public List<AudioClip> m_ArgoHelpAudio = new List<AudioClip>(64);
-	public List<string> m_ArgoHelpSubtitles = new List<string> (64);
+	public List<AudioClip> m_ArgoFunAudio = new List<AudioClip>(64);
+	public List<string> m_ArgoFunSubtitles = new List<string> (64);
 	
 	[SerializeField] private PuzzleButton m_Button;
 	[SerializeField] private SubtitleDisplay m_SubTitles;
@@ -35,16 +35,16 @@ public class ArgoHelp : SharedStateComponent {
 		
 	}
 	
-	public void ArgoHelpPressed() {
+	public void ArgoFunPressed() {
 		
 		if(!m_Button.Locked && m_Button.WasPressed) {
 			if(m_CurrentClip != -1) {
 				ArgoAudio.Stop();
-				ArgoAudio.clip = m_ArgoHelpAudio[m_CurrentClip];
+				ArgoAudio.clip = m_ArgoFunAudio[m_CurrentClip];
 				ArgoAudio.Play();
 				//AudioSource.PlayClipAtPoint(m_ArgoHelpAudio[m_CurrentClip], transform.position);
 				if(m_SubTitles != null) {
-					StartCoroutine(m_SubTitles.TypeLineString("Argo", m_ArgoHelpSubtitles[m_CurrentClip]));
+					StartCoroutine(m_SubTitles.TypeLineString("Argo", m_ArgoFunSubtitles[m_CurrentClip]));
 				}
 			}
 		}
