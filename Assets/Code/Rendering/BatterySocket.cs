db9f209548b4cf62273a1fce586340406c5e3f47
Ross <rdtredinnick@wisc.edu>
2024-04-22 09:56:05 -0500

    Tweaks from last Friday to battery puzzle and plane in level 5.

--- Assets/Code/Rendering/BatterySocket.cs
+++ Assets/Code/Rendering/BatterySocket.cs
@@ -9,17 +9,16 @@ namespace WeatherStation {
 		[SerializeField] ItemSocket Socket;
 		
 		private GameObject BatteryCover;
+		
 		// Start is called before the first frame update
 		void Awake() {
-
+			if(Socket != null) {
+				Socket.OnAdded.Register(FindCover);
+			}
 		}
 	
 
 		public void OpenCover()	 {
-			
-			if(BatteryCover == null) {
-				BatteryCover = transform.GetChild(0).GetChild(0).GetChild(1).gameObject;
-			}
 			
 			if(BatteryCover != null)
 			{
@@ -30,6 +29,27 @@ namespace WeatherStation {
 					WSAnalytics w = Find.State<WSAnalytics>();
 					if(w != null) {
 						w.LogBatteryBoxOpen();
+					}
+				}
+			}
+		}
+		
+		private void FindCover() {
+			if(BatteryCover == null) {
+				if( transform.childCount > 0) {
+					Transform c1 = transform.GetChild(0);
+					if(c1 != null) {
+						if(c1.childCount > 0) {
+							Transform c2 = c1.GetChild(0);
+							if(c2.childCount > 1) {
+								if(c2 != null) {
+									Transform c3 = c2.GetChild(1);
+									if(c3 != null) {
+										BatteryCover = c3.gameObject;
+									}
+								}
+							}
+						}
 					}
 				}
 			}
